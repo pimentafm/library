432310eb745e8074919072b29cc102b394996453
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2021-07-18 14:53:15 -0300

    Add image upload

--- books/models.py
+++ books/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from uuid import uuid4
 
 
+def upload_image_book(instance, filename):
+    return f'{instance.id_book}-{filename}'
+
+
 class Books(models.Model):
     id_book = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=255)
@@ -11,3 +15,5 @@ class Books(models.Model):
     pages = models.IntegerField()
     publishing_company = models.CharField(max_length=255)
     created_at = models.DateField(auto_now_add=True)
+    image = models.ImageField(
+        upload_to=upload_image_book, blank=True, null=True)
--- library/urls.py
+++ library/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 from rest_framework import routers
 
 from books.api import viewsets as booksviewsets
@@ -27,4 +31,4 @@ route.register(r'books', booksviewsets.BooksViewSet, basename='Books')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
